1252fd0f0705cb8ba49131a1790075dda772e9b5
kanya <seikokinetic@live.fr>
2023-05-23 06:48:51 +0200

    first commit

--- affiche.php
+++ affiche.php
@@ -1,20 +1,31 @@
 <?php
-// appeler le fichier de la connexion ---- appel de PDO.php
-// utiliser l'include
-include 'db.php';
-//variable pour appeler la fonction connexiondb()
-$con = connexionDB();
-//faire la requète 
-//création de la variable requete et on y fait une requete
+//appeler le fichier de la connexion bdd
+//utiliser include
+include "db.php";
+
+$con=connexionDB();
+
+//suppression d'un utilisateur
+if(isset($_GET['del'])){
+  $id = $_GET['del'];
+  $delRequest = "DELETE FROM utilisateur WHERE id=?";
+  $deleteRequest = $con->prepare($delRequest);
+  $deleteRequest->execute(array($id));
+  header('location : affiche.php');
+}
+
+
+
+
+//faire la requête
+//création de la variable requete et on y fait une requete sql
 $requete = 'SELECT * from utilisateur';
-//on souhaite avoir la réponse de la requete dans une variable.
-//ATTENTION on est pas encore connecté
-//on crée la variable "$response" et on se connecte à la BDD en faisant une requete sql sockée dans response
+//on souhaite dans une variable avoir la réponse de la requête
+//Attention on est pas encore co à la bdd
+//on créé la variable "$response" et on se co à la bdd 
 $response = $con->query($requete);
+//on récupère toutes les lignes de la requête
 $lignes = $response->fetchAll();
-
-//on récupère toutes les lignes de la requète
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,48 +33,42 @@ $lignes = $response->fetchAll();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Formulaire</title>
+    <!--CSS ONLY-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-    <div class='text-center mt-4 mb-4'>
-        <div>
-            <h1>Utilisateurs de la BDD</h1>
-        </div>
-        <button class="btn btn-success">Ajouter</button>
+    <div>
+        <h1 class="text-center mt-4 mb-4">Utilisateurs de la bdd</h1>
+    </div>
+    <div class="text-center mt-4 mb-4">
+        <a href="add.php"><button class="btn btn-success">Ajouter</button></a>
     </div>
 <table class="table">
   <thead>
     <tr>
       <th scope="col">id</th>
-      <th scope="col">nom</th>
-      <th scope="col">prénom</th>
+      <th scope="col">Nom</th>
+      <th scope="col">Prénom</th>
       <th scope="col">Modifier</th>
       <th scope="col">Supprimer</th>
     </tr>
   </thead>
   <tbody>
     <?php
-    //boucle for each pour afficher chaque ligne de la requete
+    //boucle "for each pour afficher chaque lignes de la requête
     foreach($lignes as $ligne){
-        echo '<tr>
-        <th scope="row">'.$ligne['id'].'</th>
-        <td>'.$ligne['nom'].'</td>
-        <td>'.$ligne['prenom'].'</td>
-        <td><button class ="btn btn-warning">Modifier</button></td>
-        <td><button class ="btn btn-danger">Supprimer</button></td>
-      </tr>';
+       echo '<tr>
+      <th scope="row">'.$ligne['id'].'</th>
+      <td>'.$ligne['nom'].'</td>
+      <td>'.$ligne['prenom'].'</td>
+      <td><a class="btn btn-warning" href="update.php?id='.$ligne['id'].'">Modifier</a></td>
+      <td><form method="GET"><button class="btn btn-danger" type="submit" name="del" value='.$ligne['id'].' >Supprimer</button></form></td>
+    </tr>';
     }
-
-    ?>
-
     
-
+    ?>
   </tbody>
 </table>
-
-
-
-
 </body>
 </html>
--- update.php
+++ update.php
@@ -1,13 +1,73 @@
 <?php
-include "DB.php";
+// appeler le fichier de la connexion bdd
+include "db.php";
+// variable pour appeler la fonction connexionDB()
 $con = connexionDB();
 
-if(isset($_POST['nom'], $_POST['prenom'], $_POST['id'])){
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $id = $_POST['id'];
+// Vérifier si l'id est fourni dans la requête GET
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
 
-    header('location: affiche.php');
-    exit();
+    // Requête pour récupérer les données de l'utilisateur spécifié par l'id
+    $requete = "SELECT * FROM utilisateur WHERE id = $id";
+    $response = $con->query($requete);
+    $user = $response->fetch(); // Fetch a single row
+
+    // Vérifier si l'utilisateur existe
+    if ($user) {
+        // Le formulaire a été soumis
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Récupérer les valeurs soumises
+            $nom = $_POST['nom'];
+            $prenom = $_POST['prenom'];
+
+            // Mettre à jour les données de l'utilisateur dans la base de données
+            $requeteUpdate = "UPDATE utilisateur SET nom = '$nom', prenom = '$prenom' WHERE id = $id";
+            $con->exec($requeteUpdate);
+
+            // Rediriger vers la page affiche.php pour afficher la liste mise à jour
+            header('Location: affiche.php');
+            exit;
+        }
+    } else {
+        // L'utilisateur n'existe pas, gérer cette situation (par exemple, afficher un message d'erreur)
+        echo 'Utilisateur non trouvé.';
+    }
+} else {
+    // L'id n'est pas fourni dans la requête GET, gérer cette situation (par exemple, afficher un message d'erreur)
+    echo 'ID non fourni.';
 }
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modifier Utilisateur</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+    <div>
+        <h1 class="text-center mt-4 mb-4">Modifier Utilisateur</h1>
+    </div>
+    <div class="container">
+        <form method="POST">
+            <div class="mb-3">
+                <label for="nom" class="form-label">Nom</label>
+                <input type="text" class="form-control" id="nom" name="nom" value="<?php echo $user['nom']; ?>">
+            </div>
+            <div class="mb-3">
+                <label for="prenom" class="form-label">Prénom</label>
+                <input type="text" class="form-control" id="prenom" name="prenom"
+                    value="<?php echo $user['prenom']; ?>">
+            </div>
+            <button type="submit" class="btn btn-primary">Enregistrer</button>
+        </form>
+    </div>
+</body>
+
+</html>
